51f7a2399af3805873f6ccf0e4f813dc599c1166
KchoPein <kchopein@gmail.com>
2017-08-31 20:57:16 +0200

    Startup and  launch.json

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AggregationService
 {
