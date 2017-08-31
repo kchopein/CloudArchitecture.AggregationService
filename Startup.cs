174ff24cfb989d00a5f4302c5845ef6b92a3d4bd
KchoPein <kchopein@gmail.com>
2017-08-31 21:00:19 +0200

    Startup

--- Startup.cs
+++ Startup.cs
@@ -14,29 +14,53 @@ using Swashbuckle.AspNetCore.Swagger;
 namespace AggregationService
 {
     public class Startup
+{
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        var builder = new ConfigurationBuilder()
+            .AddEnvironmentVariables();
+        Configuration = builder.Build();
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
+        this.hostingEnvironment = env;
     }
+
+    public IConfigurationRoot Configuration { get; }
+
+    protected IHostingEnvironment hostingEnvironment;
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddMvc()
+            .AddJsonOptions(
+                opts =>
+                {
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Info { Title = "API V1", Version = "v1" });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+        loggerFactory.AddDebug();
+
+        app.UseMvc();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+        });
+    }
+}
 }
